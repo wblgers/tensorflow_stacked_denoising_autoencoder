75ab8325a11099c25b1a011b91fd6abcde613e94
wblgers <wblk24@yeah.net>
2018-08-14 15:56:05 +0800

    Add some comments on the code, modify the AE_Softmax network structure

--- AE_Softmax_MNIST.py
+++ AE_Softmax_MNIST.py
@@ -3,9 +3,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 from library.Autoencoder import Autoencoder
 
+# Read database
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-
+# hyper parameters
 n_samples = int(mnist.train.num_examples)
 training_epochs = 20
 batch_size = 128
@@ -20,10 +21,35 @@ n_hidden = 400
 n_outputs = 10
 lr = 0.001
 
+# define the autoencoder
 ae = Autoencoder(n_layers=[n_inputs, n_hidden],
                           transfer_function = tf.nn.relu,
                           optimizer = tf.train.AdamOptimizer(learning_rate = lr),
                           ae_para = [corruption_level, sparse_reg])
+
+# define the output layer using softmax
+x = tf.placeholder(tf.float32, [None, n_hidden])
+W = tf.Variable(tf.zeros([n_hidden, n_outputs]))
+b = tf.Variable(tf.zeros([n_outputs]))
+y = tf.matmul(x, W) + b
+
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, n_outputs])
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+train_step = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy)
+
+## define the output layer using softmax in the fine tuning step
+x_ae = ae.transform()
+y_ft = tf.matmul(x_ae, W) + b
+
+cross_entropy_ft = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_ft, labels=y_))
+train_step_ft = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy_ft)
+
+correct_prediction_ft = tf.equal(tf.argmax(y_ft, 1), tf.argmax(y_, 1))
+accuracy_ft = tf.reduce_mean(tf.cast(correct_prediction_ft, tf.float32))
+
+## Initialization
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -33,7 +59,6 @@ for epoch in range(training_epochs):
     # Loop over all batches
     for i in range(total_batch):
         batch_xs, _ = mnist.train.next_batch(batch_size)
-
         # Fit training using batch data
         temp = ae.partial_fit()
         cost, opt = sess.run(temp,feed_dict={ae.x: batch_xs, ae.keep_prob : ae.in_keep_prob})
@@ -48,42 +73,23 @@ for epoch in range(training_epochs):
 ae_test_cost = sess.run(ae.calc_total_cost(),feed_dict={ae.x: mnist.test.images, ae.keep_prob : 1.0})
 print("Total cost: " + str(ae_test_cost))
 
-print("******************************************************")
-# Create the model
-x = tf.placeholder(tf.float32, [None, n_hidden])
-W = tf.Variable(tf.zeros([n_hidden, n_outputs]))
-b = tf.Variable(tf.zeros([n_outputs]))
-y = tf.matmul(x, W) + b
+print("************************Finish the autoencoder training******************************")
 
-# Define loss and optimizer
-y_ = tf.placeholder(tf.float32, [None, n_outputs])
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-train_step = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy)
-
-sess.run(tf.variables_initializer([W, b]))
-init = tf.global_variables_initializer()
-sess.run(init)
+# Training the softmax layer
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
     x_ae = sess.run(ae.transform(),feed_dict={ae.x: batch_xs, ae.keep_prob : ae.in_keep_prob})
     sess.run(train_step, feed_dict={x: x_ae, y_: batch_ys})
 
+print("************************Finish the softmax output layer training******************************")
+print("Test accuracy before fine-tune")
+print(sess.run(accuracy_ft, feed_dict={ae.x: mnist.test.images, y_: mnist.test.labels,
+                                    ae.keep_prob: 1.0}))
 
-# Fine tune
-x_ae = ae.transform()
-y = tf.matmul(x_ae, W) + b
-cross_entropy_ft = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-train_step_ft = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy_ft)
-
-init = tf.global_variables_initializer()
-sess.run(init)
+# Training of fine tune
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
     sess.run(train_step_ft,feed_dict={ae.x: batch_xs, y_: batch_ys, ae.keep_prob : ae.in_keep_prob})
-
-
+print("************************Finish the fine tuning******************************")
 # Test trained model
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={ae.x: mnist.test.images, y_: mnist.test.labels, ae.keep_prob : 1.0}))
+print(sess.run(accuracy_ft, feed_dict={ae.x: mnist.test.images, y_: mnist.test.labels, ae.keep_prob : 1.0}))
--- SAE_Softmax_MNIST.py
+++ SAE_Softmax_MNIST.py
@@ -3,8 +3,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 from library.Autoencoder import Autoencoder
 
+# Read database
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+# hyper parameters
 n_samples = int(mnist.train.num_examples)
 training_epochs = 20
 batch_size = 128
@@ -20,6 +22,7 @@ n_hidden2 = 100
 n_outputs = 10
 lr = 0.001
 
+# define the autoencoder
 ae = Autoencoder(n_layers=[n_inputs, n_hidden],
                           transfer_function = tf.nn.relu,
                           optimizer = tf.train.AdamOptimizer(learning_rate = lr),
@@ -28,10 +31,44 @@ ae_2nd = Autoencoder(n_layers=[n_hidden, n_hidden2],
                           transfer_function = tf.nn.relu,
                           optimizer = tf.train.AdamOptimizer(learning_rate = lr),
                           ae_para=[corruption_level, sparse_reg])
+# define the output layer using softmax
+x = tf.placeholder(tf.float32, [None, n_hidden2])
+W = tf.Variable(tf.zeros([n_hidden2, n_outputs]))
+b = tf.Variable(tf.zeros([n_outputs]))
+y = tf.matmul(x, W) + b
 
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, n_outputs])
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+train_step  = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy)
+
+## define the output layer using softmax in the fine tuning step
+x_ft = tf.placeholder(tf.float32, [None, n_inputs])
+h = x_ft
+
+# Go through the two autoencoders
+for layer in range(len(ae.n_layers) - 1):
+    # h = tf.nn.dropout(h, ae.in_keep_prob)
+    h = ae.transfer(
+        tf.add(tf.matmul(h, ae.weights['encode'][layer]['w']),ae.weights['encode'][layer]['b']))
+for layer in range(len(ae_2nd.n_layers) - 1):
+    # h = tf.nn.dropout(h, ae_2nd.in_keep_prob)
+    h = ae_2nd.transfer(
+        tf.add(tf.matmul(h, ae_2nd.weights['encode'][layer]['w']),ae_2nd.weights['encode'][layer]['b']))
+
+y_ft = tf.matmul(h, W) + b
+cross_entropy_ft = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_ft, labels=y_))
+
+train_step_ft  = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy_ft)
+correct_prediction = tf.equal(tf.argmax(y_ft, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+## Initialization
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+
 for epoch in range(training_epochs):
     avg_cost = 0.
     total_batch = int(n_samples / batch_size)
@@ -53,7 +90,7 @@ for epoch in range(training_epochs):
 ae_test_cost = sess.run(ae.calc_total_cost(),feed_dict={ae.x: mnist.test.images, ae.keep_prob : 1.0})
 print("Total cost: " + str(ae_test_cost))
 
-print("************************First AE******************************")
+print("************************First AE training finished******************************")
 
 
 for epoch in range(training_epochs):
@@ -75,64 +112,29 @@ for epoch in range(training_epochs):
     if epoch % display_step == 0:
         print("Epoch:", '%d,' % (epoch + 1),
               "Cost:", "{:.9f}".format(avg_cost))
-print("*************************2nd AE*****************************")
-# Create the model
-x = tf.placeholder(tf.float32, [None, n_hidden2])
-W = tf.Variable(tf.zeros([n_hidden2, n_outputs]))
-b = tf.Variable(tf.zeros([n_outputs]))
-y = tf.matmul(x, W) + b
 
-# Define loss and optimizer
-y_ = tf.placeholder(tf.float32, [None, n_outputs])
+print("************************Second AE training finished******************************")
 
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-train_step  = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy)
-
-init = tf.global_variables_initializer()
-sess.run(init)
-sess.run(tf.variables_initializer([W, b]))
-
+# Training the softmax layer
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-    h_ae1_out = sess.run(ae.transform(), feed_dict={ae.x: batch_xs, ae.keep_prob : ae.in_keep_prob})
-    h_ae2_out = sess.run(ae_2nd.transform(), feed_dict={ae_2nd.x: h_ae1_out, ae_2nd.keep_prob : ae_2nd.in_keep_prob})
+    h_ae1_out = sess.run(ae.transform(), feed_dict={ae.x: batch_xs, ae.keep_prob : 1.0})
+    h_ae2_out = sess.run(ae_2nd.transform(), feed_dict={ae_2nd.x: h_ae1_out, ae_2nd.keep_prob : 1.0})
     sess.run(train_step, feed_dict={x: h_ae2_out, y_: batch_ys})
-print("*************************Softmax*****************************")
+print("*************************Finish the softmax output layer training*****************************")
 
-# Fine tune
-x_ft = tf.placeholder(tf.float32, [None, n_inputs])
-# ae.setNewX(x_ft)
-# ae_2nd.setNewX(ae.transform())
-# h = ae_2nd.transform()
-h = x_ft
-for layer in range(len(ae.n_layers) - 1):
-    # h = tf.nn.dropout(h, ae.in_keep_prob)
-    h = ae.transfer(
-        tf.add(tf.matmul(h, ae.weights['encode'][layer]['w']),ae.weights['encode'][layer]['b']))
-for layer in range(len(ae_2nd.n_layers) - 1):
-    # h = tf.nn.dropout(h, ae_2nd.in_keep_prob)
-    h = ae_2nd.transfer(
-        tf.add(tf.matmul(h, ae_2nd.weights['encode'][layer]['w']),ae_2nd.weights['encode'][layer]['b']))
-
-y_ft = tf.matmul(h, W) + b
-cross_entropy_ft = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_ft, labels=y_))
-
-train_step_ft  = tf.train.AdamOptimizer(learning_rate = lr).minimize(cross_entropy_ft)
-correct_prediction = tf.equal(tf.argmax(y_ft, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print("Test accuracy before fine-tune")
 print(sess.run(accuracy, feed_dict={x_ft: mnist.test.images, y_: mnist.test.labels,
                                     ae.keep_prob: 1.0,ae_2nd.keep_prob : 1.0}))
 
-init = tf.global_variables_initializer()
-sess.run(init)
+# Training of fine tune
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
     sess.run(train_step_ft,feed_dict={x_ft: batch_xs, y_: batch_ys,
-                                      ae.keep_prob: ae.in_keep_prob, ae_2nd.keep_prob: ae_2nd.in_keep_prob})
+                                      ae.keep_prob: 1.0, ae_2nd.keep_prob: 1.0})
 
-print("*************************Fine tune*****************************")
+print("************************Finish the fine tuning******************************")
 # Test trained model
 print(sess.run(accuracy, feed_dict={x_ft: mnist.test.images, y_: mnist.test.labels,
                                     ae.keep_prob: 1.0, ae_2nd.keep_prob: 1.0}))
